c940bfa3e11cf210e9930ecf1dcbeb7562bdecce
miki232 <michele.toia30@gmail.com>
2022-08-08 00:58:05 +0200

    Add files via upload

---
+++ ray.h
@@ -0,0 +1,52 @@
+#ifndef RAY_H
+# define RAY_H
+#include <unistd.h>
+
+typedef struct s_ray
+{
+    void    *mlx_ptr;
+    void    *win_ptr;
+    void    *img;
+    void    *img2;
+    void    *img3;
+    void    *img4;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+    void    *background;
+    void    *img4s;
+    int     y;
+	int		gliss;
+    int     x;
+    int     ssa;
+    char     color;
+	int w,a,d,s;
+}           t_ray;
+
+
+
+typedef enum e_keys
+{
+	KB_PAGE_UP = 116,
+	KB_PAGE_DOWN = 121,
+	W = 13,
+	A = 0,
+	S = 1,
+	D = 2,
+	ESC = 53,
+	PLUS = 69,
+	MINUS = 78,
+	RIGHT = 123,
+	LEFT = 124,
+	UP = 125,
+	DOWN = 126,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+}	t_keys;
+
+#endif
